03fffe5ae8246e8fb94ac6a5dd09365c9adb36e3
Takako Onozuka <onozukatakako@gmail.com>
2023-02-20 22:28:43 +0000

    added container class

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -5,11 +5,13 @@ import Stack from 'react-bootstrap/Stack';
 const ProjectList = () => {
     return ( 
         <>  
+    <div class="container">    
         <h3>Project List</h3>
     <Stack direction="horizontal" gap={3}>
     <Project />
     <Project />
     </Stack>
+    </div>
     </>
 
      );

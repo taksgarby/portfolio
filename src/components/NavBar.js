914b3770691ffccfe78a2ce576c17aad35daf9f2
Takako Onozuka <onozukatakako@gmail.com>
2023-02-20 18:01:57 +0000

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,29 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
 const NavBar = () => {
-    return ( 
-        <h3>NavBar</h3>
-     );
+
+    return (
+        <>
+          <Navbar bg="info" variant="light">
+            <Container>
+              <Navbar.Brand href="#welcome">Welcome</Navbar.Brand>
+              <Nav className="me-auto">
+                <Nav.Link href="#aboutme">About Me</Nav.Link>
+                <Nav.Link href="#projects">Projects</Nav.Link>
+                <Nav.Link href="#contact">Contact</Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
+         
+        </>
+      );
+
+
+
+    
 }
  
 export default NavBar;
-<h3>NavBar</h3>

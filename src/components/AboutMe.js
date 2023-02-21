559ee6e517dd1a98ea8d0644b545d06efa22bd62
Takako Onozuka <onozukatakako@gmail.com>
2023-02-21 09:52:48 +0000

    added container styling

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,22 +1,48 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import './components.css';
 
-
-const AboutMe = () => {
+function AboutMe() {
   return (
-    <Container>
-      <Row>
-        <Col sm={4}>name</Col>
-        <Col sm={8}>details</Col>
-      </Row>
-    
-
-    </Container>
+    <div class="container">    
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+    </div>
   );
 }
 
 export default AboutMe;
+
+
+
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+
+
+// const AboutMe = () => {
+//   return (
+//     <Container>
+//       <Row>
+//         <Col sm={4}>name</Col>
+//         <Col sm={8}>details</Col>
+//       </Row>
+    
+
+//     </Container>
+//   );
+// }
+
+// export default AboutMe;
 
 
 

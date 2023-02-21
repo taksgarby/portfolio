e03bd9d7408fdae61bf7c37761887612a44b53e8
Takako Onozuka <onozukatakako@gmail.com>
2023-02-21 11:37:31 +0000

    more styling

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,6 @@
 import Card from 'react-bootstrap/Card';
 import './components.css';
+import Button from 'react-bootstrap/Button';
 
 function AboutMe() {
   return (
@@ -11,29 +12,56 @@ function AboutMe() {
         <div class="full-name">Takako Onozuka</div>
         {/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
         <p class="h3">Junior Full Stack Developer</p>
-        <Card.Text class="skills">
-          Java | JavaScript | Python | HTML | CSS | MySQL | MongoDB
-        </Card.Text>
+        {/* <Card.Text class="skills"> */}
+          {/* Java | JavaScript | Python | HTML | CSS | MySQL | MongoDB */}
+        {/* </Card.Text> */}
+        <div class="button-container-language">
+        <button type="button" class="btn btn-info">Java</button>
+        <button type="button" class="btn btn-info">JavaScript</button>
+        <button type="button" class="btn btn-info">Python</button>
+        <button type="button" class="btn btn-info">HTML/CSS</button>
+        </div>
+        <div class="button-container-others">
+        <button type="button" class="btn btn-info">MySQL</button>
+        <button type="button" class="btn btn-info">MongoDB</button>
+        <button type="button" class="btn btn-info">Agile</button>
+        <button type="button" class="btn btn-info">TDD</button>
+        </div>
+        {/* <Button style={{ width: '5rem', height: '2rem'}} className="m-2">Java</Button>
+        <Button>JavaScript</Button>
+        <Button>Python</Button>
+        <Button>MySQL</Button>
+        <Button>MongoDB</Button>
+        <Button>Agile</Button> */}
+        <div class="link-container">
         <Card.Link href="#">Card Link</Card.Link>
         <Card.Link href="#">Another Link</Card.Link>
+        </div>
       </Card.Body>
     </Card>
 
     <Card style={{ width: '30rem' }} className="m-5">
         <Card.Body>
           {/* <Card.Title >Card Title</Card.Title> */}
-          <h1>Takako Onozuka</h1>
-          <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+          <div class="aboutme-right">
+            <p class="aboutme-headline">
+                adkalfdjakldjakldfajkdls
+            </p>
+            <p class="aboutme-texts">
+                adfjakldjfaldfajkl;
+            </p>
+          {/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
-          </Card.Text>
+          </Card.Text>s
           <Card.Link href="#">Card Link</Card.Link>
-          <Card.Link href="#">Another Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link> */}
+             </div>
         </Card.Body>
       </Card>
-    </div>
-
+ 
+</div>
   );
 }
 

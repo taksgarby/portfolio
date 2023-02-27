f6d71507f635f8616114be7b5813f8d10711240e
Takako Onozuka <onozukatakako@gmail.com>
2023-02-27 16:57:45 +0000

    fixed navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,38 +1,65 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import './components.css';
-import Contact from './Contact';
-import React from 'react';
-import ProjectList from './ProjectList';
+import {RiBearSmileLine} from 'react-icons/ri';
+import {FaDesktop} from 'react-icons/fa';
+import {GrContact} from 'react-icons/gr'
 
-const NavBar = () => {
 
-    return (
-        <>
-          <Navbar bg="info" variant="light">  
-            <Container>
+function NavBar() {
+  return (
+    <>
+      <Navbar bg="info" variant="light">
+        <Container>
+          <Navbar.Brand href="#home" > Welcome!</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#projects"><FaDesktop /> Projects</Nav.Link>
+            <Nav.Link href="#contact"><GrContact /> Contact</Nav.Link>
+
+          </Nav>
+        </Container>
+      </Navbar>
+  
+  </>
+  )
+  };
+      export default NavBar;
+
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import './components.css';
+// import Contact from './Contact';
+// import React from 'react';
+// import ProjectList from './ProjectList';
+
+// const NavBar = () => {
+
+//     return (
+//         <>
+//           <Navbar bg="info" variant="light">  
+//             <Container>
             
-              <Navbar.Brand class="nav-text">Welcome to my portfolio page!</Navbar.Brand>
+//               <Navbar.Brand class="nav-text">Welcome to my portfolio page!</Navbar.Brand>
            
-              <div class="nav-welcome">
-              <Nav className="me-auto">
-                {/* <Nav.Link href="#aboutme">About Me</Nav.Link>
-                <Nav.Link href="#projects">Projects</Nav.Link> */}
-                <Nav.Link href="#projects" element={<ProjectList />}>Projects</Nav.Link>
-                <Nav.Link href="#contact" element={<Contact />}>Contact</Nav.Link>
-              </Nav>
-              </div>
-            </Container>
-            </Navbar>
+//               <div class="nav-welcome">
+//               <Nav className="me-auto">
+//                 {/* <Nav.Link href="#aboutme">About Me</Nav.Link>
+//                 <Nav.Link href="#projects">Projects</Nav.Link> */}
+//                 <Nav.Link href="#projects" element={<ProjectList />}>Projects</Nav.Link>
+//                 <Nav.Link href="#contact" element={<Contact />}>Contact</Nav.Link>
+//               </Nav>
+//               </div>
+//             </Container>
+//             </Navbar>
       
          
-        </>
-      );
+//         </>
+//       );
 
 
 
     
-}
+// }
  
-export default NavBar;
+// export default NavBar;
